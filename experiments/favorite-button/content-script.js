86db0da494aeed3671a7bc3aa31c23a6984a683e
Victor Lin <vvictor.llin@gmail.com>
2021-01-01 01:11:07 -0800

    attempt to alter store text on load

--- experiments/favorite-button/content-script.js
+++ experiments/favorite-button/content-script.js
@@ -1,4 +1,47 @@
-var div=document.createElement("div"); 
-document.body.appendChild(div); 
-div.innerText="test123";
+conditions = [
+    (e) => e.id == "root",
+    (e) => e.nodeName == "DIV" && e.className == "",
+    (e) => e.className == "sc-gzVnrw ipJBfZ",
+    (e) => e.className == "sc-bYSBpT fEJYvd",
+    (e) => e.className == "sc-gxMtzJ hCivtC",
+    (e) => e.className == "sc-hpAzQi gdkNys",
+    (e) => e.nodeName == "HEADER",
+    (e) => e.className == "sc-chAAoq gqyxRS",
+    (e) => e.nodeName == "H1",
+];
 
+nestedObserver = async (conditions, callback) => {
+    let observer
+
+    const listeners = conditions.map(
+      condition => parent =>
+        new Promise((resolve) => {
+          
+          for (const child of parent.children){
+              if (condition(child)) resolve(child);
+          }
+  
+          observer = new MutationObserver((mutations) => {
+              for (let mr of mutations) {
+                  if (condition(mr.target)) {
+                      resolve(mr.target);
+                  }
+              }
+          });
+  
+          observer.observe(parent, { childList: true }, {subTree: true});
+        })
+    );
+    
+    var parent = document.body
+    for (const listener of listeners){
+      parent = await listener(parent)
+      alert(parent.className)
+      observer.disconnect()
+    }
+  
+    callback(parent)
+  };
+  
+
+nestedObserver(conditions, el => console.log(el.id))
