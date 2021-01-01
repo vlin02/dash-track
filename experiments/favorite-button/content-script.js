19a6ec8d16366140f8e703c4317e9ba1cb2c3b2f
Victor Lin <vvictor.llin@gmail.com>
2021-01-01 01:51:15 -0800

    hijacked store name with classname scan
    Originally didn't work because script continuously reupdated name
    on each mutation. React didn't like that and created an infinite
    loop. Fixed by only updating if name is already set to "Hijacked"

--- experiments/favorite-button/content-script.js
+++ experiments/favorite-button/content-script.js
@@ -1,47 +1,13 @@
-conditions = [
-    (e) => e.id == "root",
-    (e) => e.nodeName == "DIV" && e.className == "",
-    (e) => e.className == "sc-gzVnrw ipJBfZ",
-    (e) => e.className == "sc-bYSBpT fEJYvd",
-    (e) => e.className == "sc-gxMtzJ hCivtC",
-    (e) => e.className == "sc-hpAzQi gdkNys",
-    (e) => e.nodeName == "HEADER",
-    (e) => e.className == "sc-chAAoq gqyxRS",
-    (e) => e.nodeName == "H1",
-];
+observer = new MutationObserver(() => {
+  const titleDiv = [].concat(
+      Array.from(document.getElementsByClassName("sc-dTLGrV gYYmdm sc-kVyEtE efQmPr")),
+      Array.from(document.getElementsByClassName("sc-dTLGrV gYYmdm sc-dyKSPo gutEGH")),
+  )[0]
+  
+  if (titleDiv !== undefined && titleDiv.innerText !== "Hijacked") {
+    titleDiv.innerText="Hijacked"
+    console.log('found')
+  }
+});
 
-nestedObserver = async (conditions, callback) => {
-    let observer
-
-    const listeners = conditions.map(
-      condition => parent =>
-        new Promise((resolve) => {
-          
-          for (const child of parent.children){
-              if (condition(child)) resolve(child);
-          }
-  
-          observer = new MutationObserver((mutations) => {
-              for (let mr of mutations) {
-                  if (condition(mr.target)) {
-                      resolve(mr.target);
-                  }
-              }
-          });
-  
-          observer.observe(parent, { childList: true }, {subTree: true});
-        })
-    );
-    
-    var parent = document.body
-    for (const listener of listeners){
-      parent = await listener(parent)
-      alert(parent.className)
-      observer.disconnect()
-    }
-  
-    callback(parent)
-  };
-  
-
-nestedObserver(conditions, el => console.log(el.id))
+observer.observe(document.body, { childList: true, subtree: true });
