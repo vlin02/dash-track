4ea409f9dd0f89b95bdff6db4b5f8222d06e70c6
Victor Lin <vvictor.llin@gmail.com>
2021-01-02 01:06:19 -0800

    add unfavorite button and toggling
    Favorite button is essentially complete. Moving on to phase 2:
    viewing all favorited restaurants

--- scripts/favorite-button/content-script.js
+++ scripts/favorite-button/content-script.js
@@ -1,41 +1,72 @@
 const injectFavoriteButton = new MutationObserver(() => {
   const storeName = $(".sc-dTLGrV.gYYmdm")
+  const schema = $('script[type="application/ld+json"]:contains(@context)')
 
-  if (storeName !== undefined) {
-    if (storeName.prop("outerHTML") !== undefined) {
-      onButtonClick = () => {
-        const context = JSON.parse(
-          $('script[type="application/ld+json"]:contains(@context)').html()
-        )
+  if (
+    storeName !== undefined &&
+    storeName.prop("outerHTML") !== undefined &&
+    schema.length > 0
+  ) {
+    const context = JSON.parse(schema.html())
 
-        let favoritesList = []
-        chrome.storage.sync.get('favorites', (res) => {
-          if (res.favorites !== undefined) favoritesList = res.favorites
-        })
+    let favoritesList = []
+    let isFavorited = () => favoritesList.some((rsnt) => rsnt['@id'] === context['@id'])
 
-        if (!favoritesList.some((rsnt) => rsnt.name === context.name)) {
+    chrome.storage.sync.get("favorites", (res) => {
+      if (res.favorites !== undefined) favoritesList = res.favorites
+
+      addFavorite = () => {
+        if (!isFavorited()) {
           favoritesList.push(context)
+        } else {
+          alert("This restaurant is already favorited")
         }
 
-        chrome.storage.sync.set({favorites: favoritesList})
+        chrome.storage.sync.set({ favorites: favoritesList })
+        addFavoriteButton.hide()
+        removeFavoriteButton.show()
+      }
+
+      removeFavorite = () => {
+        if (isFavorited()) {
+          favoritesList = favoritesList.filter(
+            (rsnt) => rsnt['@id'] !== context['@id']
+          )
+        } else {
+          alert("This restaurant is already unfavorited")
+        }
+
+        chrome.storage.sync.set({ favorites: favoritesList })
+        removeFavoriteButton.hide()
+        addFavoriteButton.show()
+      }
+
+      const addFavoriteButton = $("<button/>", {
+        class: "btn btn-outline-danger",
+        html: 'Favorite <i class="far fa-heart"/>',
+        click: addFavorite,
+      })
+
+      const removeFavoriteButton = $("<button/>", {
+        class: "btn btn-danger",
+        html: 'Unfavorite <i class="fas fa-heart-broken"/>',
+        click: removeFavorite,
+      })
+
+      if (isFavorited()) {
+        addFavoriteButton.hide()
+      } else {
+        removeFavoriteButton.hide()
       }
 
       const nameContainer = $("<div/>", {
         class: "d-inline-flex w-100 justify-content-between",
-      }).append([
-        storeName.clone(),
-        $("<button/>", {
-          class: "btn btn-danger",
-          html:
-            'Add to favorites <img src="https://icongr.am/fontawesome/star.svg?size=17&color=ffffff">',
-          click: onButtonClick,
-        }),
-      ])
+      }).append([storeName.clone(), removeFavoriteButton, addFavoriteButton])
 
       storeName.replaceWith(nameContainer)
 
       injectFavoriteButton.disconnect()
-    }
+    })
   }
 })
 
