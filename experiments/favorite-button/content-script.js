041a52e486cd2dacf9cd1ca2e3e52596f8f72438
Victor Lin <vvictor.llin@gmail.com>
2021-01-01 19:28:17 -0800

    reposition favorite button - completed

--- experiments/favorite-button/content-script.js
+++ experiments/favorite-button/content-script.js
@@ -2,19 +2,17 @@ const observer = new MutationObserver(() => {
   const storeName = $(".sc-dTLGrV.gYYmdm")
 
   if (storeName !== undefined) {
-    if (storeName.text !== "Hijacked") {
-      storeName.text("Hijacked")
-      storeName.before(
+    if (storeName.prop("outerHTML") !== undefined) {
+      storeName.replaceWith(
         $.parseHTML(`
-          <div class="d-inline-flex w-100 justify-content-between'>
+          <div class="d-inline-flex w-100 justify-content-between">
+            ${storeName.prop("outerHTML")}  
             <button class="btn btn-danger">
               Add to favorites <img src="https://icongr.am/fontawesome/star.svg?size=17&color=ffffff">
             </button>
-            ${storeName.prop('outerHTML')}
           </div>
         `)
       )
-    } else {
       observer.disconnect()
     }
   }
