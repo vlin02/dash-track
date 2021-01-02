f5a8822f95d180c94993c50ed403ed3fbd04df76
Victor Lin <vvictor.llin@gmail.com>
2021-01-01 23:23:58 -0800

    favorite button stores restaurant context on click

--- experiments/favorite-button/content-script.js
+++ experiments/favorite-button/content-script.js
@@ -1,21 +1,54 @@
-const observer = new MutationObserver(() => {
+$("body").append(
+  $("<script/>", {
+    id: "jq",
+    src: "https://code.jquery.com/jquery-3.3.1.min.js",
+  })
+)
+
+chrome.storage.sync.get('favorites', (res) => {
+  console.log(res.favorites)
+})
+
+
+const injectFavoriteButton = new MutationObserver(() => {
   const storeName = $(".sc-dTLGrV.gYYmdm")
 
   if (storeName !== undefined) {
     if (storeName.prop("outerHTML") !== undefined) {
-      storeName.replaceWith(
-        $.parseHTML(`
-          <div class="d-inline-flex w-100 justify-content-between">
-            ${storeName.prop("outerHTML")}  
-            <button class="btn btn-danger">
-              Add to favorites <img src="https://icongr.am/fontawesome/star.svg?size=17&color=ffffff">
-            </button>
-          </div>
-        `)
-      )
-      observer.disconnect()
+      onButtonClick = () => {
+        const context = JSON.parse(
+          $('script[type="application/ld+json"]:contains(@context)').html()
+        )
+
+        let favoritesList = []
+        chrome.storage.sync.get('favorites', (res) => {
+          if (res.favorites !== undefined) favoritesList = res.favorites
+        })
+
+        if (!favoritesList.some((rsnt) => rsnt.name === context.name)) {
+          favoritesList.push(context)
+        }
+
+        chrome.storage.sync.set({favorites: favoritesList})
+      }
+
+      const nameContainer = $("<div/>", {
+        class: "d-inline-flex w-100 justify-content-between",
+      }).append([
+        storeName.clone(),
+        $("<button/>", {
+          class: "btn btn-danger",
+          html:
+            'Add to favorites <img src="https://icongr.am/fontawesome/star.svg?size=17&color=ffffff">',
+          click: onButtonClick,
+        }),
+      ])
+
+      storeName.replaceWith(nameContainer)
+
+      injectFavoriteButton.disconnect()
     }
   }
 })
 
-observer.observe(document.body, { childList: true, subtree: true })
+injectFavoriteButton.observe(document.body, { childList: true, subtree: true })
