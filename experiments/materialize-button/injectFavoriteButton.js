f25625678e50633d0ce99940d7bd28d2dc351819
Victor Lin <vvictor.llin@gmail.com>
2021-01-05 02:39:44 -0800

    replaced url change detection with title change

---
+++ experiments/materialize-button/injectFavoriteButton.js
@@ -0,0 +1,28 @@
+$("head").append(
+  $.parseHTML(
+    '<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">'
+  )
+)
+
+const injectFavoriteButton = (vendor, title_pattern, inject_query) => {
+  new MutationObserver(() => {
+    const schema = $('script[type="application/ld+json"]:contains(@context)')
+    const favWrapper = $(".favorite-wrapper")
+    const injectDiv = $(inject_query)
+
+    if (schema.length !== 0 && favWrapper.length == 0 && injectDiv.length) {
+      const context = JSON.parse(schema.html())
+
+      if ($('title').text().match(title_pattern)) {
+        renderFavoriteButton(vendor, {
+          id: context["@id"],
+          name: context["name"]
+        }).then((favButton) => {
+          injectDiv.append(favButton)
+        })
+      }
+    } else if (favWrapper.length > 1) {
+      favWrapper.first().remove()
+    }
+  }).observe(document.body, { childList: true, subtree: true })
+}
