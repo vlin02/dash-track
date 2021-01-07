780bce06a5c8480e787191796f4c47a8753110bf
Victor Lin <vvictor.llin@gmail.com>
2021-01-06 16:30:52 -0800

    design restaurant card in dashboard

--- src/dashboard/storeCard.js
+++ src/dashboard/storeCard.js
@@ -1,14 +1,23 @@
-$(document).ready(() => {
-  const card = $.parseHTML(`
+const storeCard = () => {
+  const card = `
     <div class="card">
     <div class="card-image waves-effect waves-block waves-light">
-      <img src="https://d1ralsognjng37.cloudfront.net/a6b4b1b6-efa0-4ae0-9803-d9e42959bb29.jpeg">
+      <a href="https://www.ubereats.com/san-francisco/food-delivery/sumo-sushi-boat/1MKbP3u1Q5WsnDhs6WRwvA">
+        <img class="center-cropped" src="https://d1ralsognjng37.cloudfront.net/a6b4b1b6-efa0-4ae0-9803-d9e42959bb29.jpeg">
+      </a>
+      <a class="fav-btn">
+        <i class="material-icons" style="font-size: 27px;">favorite</i>
+      </a>
     </div>
     <div class="card-content">
-      <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
-      <p><a href="#">This is a link</a></p>
+      <span class="card-title grey-text text-darken-4">
+        <strong>Sumo Sushi Boat</strong>
+        <span class="right valign-wrapper grey-text text-darken-2" style="font-size:15px">
+            5<i class="material-icons yellow-text text-darken-2">bookmark</i>
+        </span>
+      </span>
+      <p>Added Dec 7, 2020</p>
     </div>
-  `)
-
-  $("body").append(card)
-})
+  `
+  return card
+}
---
+++ src/dashboard/storeView.js
@@ -0,0 +1,9 @@
+const gridView = $('<div/>', {
+    class: 'row'
+}).append([
+    $('<div/>', {
+        class: "col s4",
+        html: storeCard()
+    })
+])
+
